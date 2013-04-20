e599963fa3a1308bba56ce17a265c65727e41275
Marshall Farrier <info@marshallfarrier.com>
2013-04-20 15:57:47 -0700

    Set up regular expressions to use

--- bin/extract_element.py
+++ bin/extract_element.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python3.3
 import sys
+import re
+
 DIRECTORY = '../data/'
 # values to look for in file
 ELEMENT_ID = 'rightcol'
@@ -11,6 +13,43 @@ infile = DIRECTORY + sys.argv[1]
 content = ''
 with open(infile, 'r') as f:
     content = f.read()
+
+def remove_after_close(content):
+    """Return content trimmed after the opening tag has been closed
+
+    Finds the close of the first XML tag found and strips all 
+    subsequent content.
+
+    Args:
+        content: the content to be searched
+
+    Returns:
+        The trimmed content string
+    """
+    # Basic validation
+    assert content.startswith('<')
+    assert '>' in content
+
+    # Determine tag to look for
+    tag_end = content.find('>')
+    tag = content[1:tag_end]
+    tag = tag.split()[0]
+    
+    # Set up a net count for opening and closing tags
+    net_tag_count = 1
+    current_index = len(tag)
+    opening_pattern = re.compile('\<' + tag + '[> ]')
+    matches = opening_pattern.findall(content)
+    print("%d opening matches found." % len(matches))
+    closing_pattern = re.compile('\<\/' + tag + '\>')
+    closing_matches = closing_pattern.findall(content)
+    print("%d closing matches found." % len(closing_matches))
+    for m in closing_matches:
+        print(m)
+    closing_str = '</' + tag + '>'
+    end_index = 0
+    #while net_tag_count > 0:
+        
 
 def get_element_by_id(element_id, content):
     """Extract an element from a block of html code
@@ -31,9 +70,7 @@ def get_element_by_id(element_id, content):
     """
     start_index = content.find('id="' + ELEMENT_ID + '"')
     start_index = content[:start_index].rfind('<')
-    content = content[start_index:]
-    tag_end = content.find(' ')
-    tag = content[1:tag_end]
-    print('tag is "' + tag + '"')
+    return remove_after_close(content[start_index:])
 
 get_element_by_id(ELEMENT_ID, content)
+#remove_after_close('<blah this is cra-cra!> ')
